dd97ada645bbb281fc21af4cf3f010f2d491445d
Jitendra Sharma <jacksharma2177@gmail.com>
2026-07-01 15:00:56 +0530

    simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-//fixing the issue of tailwindcss not working with vite
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./", // Set the base path to the current directory
+  plugins: [react()],
+  base: "./",
 });
