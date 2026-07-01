82ca91696f6ddfc1a124630778f2cb4473e5ebd8
Jitendra Sharma <jacksharma2177@gmail.com>
2026-07-01 12:04:52 +0530

    force add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+//fixing the issue of tailwindcss not working with vite
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
